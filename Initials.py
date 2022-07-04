ea38c688524518485de04c97efe726c13c6ae344
Alessandra Lerteri <56674490+Leerti@users.noreply.github.com>
2022-07-04 17:41:32 +0200

    Update Initials.py
    Refactoring and add C

--- Initials.py
+++ Initials.py
@@ -1,10 +1,9 @@
-#Alessandra Lerteri
+#Alessandra Lerteri Caroletta
 #Since i was 14 yo i always dye my hair (:
-print("""
-  A       L
- A A      L
-A   A     L
-AAAAA     L
-A   A     L
-A   A     L
-A   A     LLLLL""")
+print("""  A       L          CCC """)
+print(""" A A      L         C   C""")
+print("""A   A     L         C    """)
+print("""AAAAA     L         C    """)
+print("""A   A     L         C    """)
+print("""A   A     L         C   C""")
+print("""A   A     LLLLL      CCC """)
